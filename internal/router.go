c40413655e906700d5f09a508441cf02f7976bf0
Jonas Kaninda <jonaskaninda@gmail.com>
2025-06-22 05:34:28 +0200

    merge v0.2

--- internal/router.go
+++ internal/router.go
@@ -34,7 +34,7 @@ func (g *Gateway) NewRouter() Router {
 		mux:           mux.NewRouter().StrictSlash(g.EnableStrictSlash),
 		enableMetrics: g.EnableMetrics,
 	}
-	gateway.addGlobalHandler(rt.mux)
+	g.addGlobalHandler(rt.mux)
 	return rt
 }
 
