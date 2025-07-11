56e55b8ce2d86908904675a92be4fdf8a078b0e1
Jonas Kaninda <jonaskaninda@gmail.com>
2025-07-11 04:49:37 +0200

    refactor: refactoring of code

--- internal/var.go
+++ internal/var.go
@@ -20,7 +20,6 @@ package internal
 import (
 	"github.com/jkaninda/goma-gateway/internal/certmanager"
 	logger2 "github.com/jkaninda/logger"
-	"time"
 )
 
 var (
@@ -34,5 +33,5 @@ var (
 	webSecureAddress   = ":8443"
 	logger             = logger2.Default()
 	certManager        *certmanager.CertManager
-	waitDelay          = 5 * time.Second
+	// waitDelay          = 5 * time.Second
 )
